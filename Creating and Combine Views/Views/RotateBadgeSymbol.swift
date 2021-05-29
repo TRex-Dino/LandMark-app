3403b05ed1b719e0997cbf553b41825d2d958402
Dima <dmitrysinkov6@gmail.com>
2021-05-29 21:35:59 +0300

    part 4.3 com

---
+++ Creating and Combine Views/Views/RotateBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotateBadgeSymbol.swift
+//  Creating and Combine Views
+//
+//  Created by Dmitry on 29.05.2021.
+//
+
+import SwiftUI
+
+struct RotateBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotateBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotateBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
