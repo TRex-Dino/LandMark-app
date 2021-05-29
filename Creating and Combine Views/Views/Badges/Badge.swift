fe0d22583f3236a7af4c310f114ad5fa3205d3db
Dima <dmitrysinkov6@gmail.com>
2021-05-29 22:46:04 +0300

    part 4 is complited

---
+++ Creating and Combine Views/Views/Badges/Badge.swift
@@ -0,0 +1,40 @@
+//
+//  Badge.swift
+//  Creating and Combine Views
+//
+//  Created by Dmitry on 29.05.2021.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount) * 360)
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1 / 4, anchor: .top)
+                    .position(x: geometry.size.width / 2, y: (3 / 4) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
